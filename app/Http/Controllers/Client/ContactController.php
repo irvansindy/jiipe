5e0e01d3cee7b8f9ee44c700c96a98dd677f683c
irvansindy <irvanmuhammad22@gmail.com>
2025-12-14 15:24:29 +0700

    fix integration data contact

--- app/Http/Controllers/Client/ContactController.php
+++ app/Http/Controllers/Client/ContactController.php
@@ -3,12 +3,63 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\ContactOverview;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        return view('layouts.client.contact.index');
+        // Cache data selama 24 jam (1440 menit)
+        $locale = app()->getLocale();
+        $cacheKey = "contact_overview_{$locale}";
+
+        $contactData = Cache::remember($cacheKey, now()->addDay(), function () use ($locale) {
+            return ContactOverview::with(['translations' => function ($query) use ($locale) {
+                $query->where('locale', $locale);
+            }])
+            ->first();
+        });
+
+        // Fallback jika tidak ada data
+        if (!$contactData || $contactData->translations->isEmpty()) {
+            $contactData = $this->getDefaultContactData();
+        } else {
+            // Ambil translation pertama
+            $contactData->translation = $contactData->translations->first();
+        }
+        return view('layouts.client.contact.index', compact('contactData'));
+    }
+
+    /**
+     * Get default contact data jika database kosong
+     */
+    private function getDefaultContactData()
+    {
+        return (object) [
+            'image' => 'asset/images/static/3ec59cba31JIIPE Tower up.jpg',
+            'translation' => (object) [
+                'title' => 'Contact Us',
+                'subtitle' => 'We are here to support you',
+                'description' => 'Please contact our hotline for further information (Monday - Friday, 08.00 - 17.00 local time). For inquiries above operating hours, kindly fill in the contact form and we will be in touch with you at our earliest.',
+                'office_name' => 'PT. Berkah Kawasan Manyar Sejahtera',
+                'phone' => '+62 31 985 409 99',
+                'address' => 'Jl. Raya Manyar Km. 11, Manyar-Gresik, East Java 61151',
+                'map_link' => 'https://www.google.com/maps/place/JIIPE+Office/@-7.0856958,112.6046212,17z/data=!4m5!3m4!1s0x0:0xaee7dea2e8cb80a3!8m2!3d-7.0862954!4d112.6030116'
+            ]
+        ];
+    }
+
+    /**
+     * Clear cache (untuk admin panel)
+     */
+    public function clearCache()
+    {
+        Cache::forget('contact_overview_en');
+        Cache::forget('contact_overview_id');
+        // Tambahkan locale lain jika ada
+
+        return redirect()->back()->with('success', 'Contact cache cleared successfully');
     }
 }
--- resources/views/layouts/client/contact/index.blade.php
+++ resources/views/layouts/client/contact/index.blade.php
@@ -43,30 +43,34 @@
         </div>
     </section>
 
-    {{-- Cover Image --}}
+    {{-- Cover Image - Dynamic --}}
     <section class="cover-profil cover-contact lazy-bg"
-        style="background-image: url('{{ asset('asset/images/static/3ec59cba31JIIPE Tower up.jpg') }}');">
+        style="background-image: url('{{ asset('uploads/contact/overview/'.$contactData->image ?? 'asset/images/static/3ec59cba31JIIPE Tower up.jpg') }}');">
         <div class="prelative container">
             <div class="row"></div>
         </div>
     </section>
 
-    {{-- Contact Information --}}
+    {{-- Contact Information - Dynamic --}}
     <section class="profil-sec-1">
         <div class="prelative container">
             <div class="row utama">
                 <div class="col-md-15">
                     <img src="{{ asset('asset/images/beijing-red.png') }}" alt="kawasan industri gresik jiipe">
-                    <p class="info">Contact Us</p>
+                    <p class="info">{{ $contactData->translation->title ?? 'Contact Us' }}</p>
                 </div>
 
                 <div class="col-md-45">
                     <div class="judul mb-4 pb-4">
-                        <h2>We are here to support you</h2>
-                        <h5>Please contact our hotline for further information (Monday - Friday, 08.00 - 17.00 local time)
-                        </h5>
-                        <h5>For inquiries above operating hours, kindly fill in the contact form and we will be in touch
-                            with you at our earliest.</h5>
+                        <h2>{{ $contactData->translation->subtitle ?? 'We are here to support you' }}</h2>
+                        @if ($contactData->translation->description)
+                            {!!$contactData->translation->description !!}
+                        @else
+                            <h5>Please contact our hotline for further information (Monday - Friday, 08.00 - 17.00 local
+                                time)</h5>
+                            <h5>For inquiries above operating hours, kindly fill in the contact form and we will be in touch
+                                with you at our earliest.</h5>
+                        @endif
                     </div>
 
                     <div id="contact-form1" class="contacts_tx_info mb-5 pb-3 pt-1">
@@ -79,11 +83,14 @@
                                     <div class="sn_text d-inline-block align-top pl-1">
                                         <span class="pb-3 d-block">Headquarters</span>
                                         <p class="pt-1">
-                                            <b>PT. Berkah Kawasan Manyar Sejahtera</b><br>
-                                            Jl. Raya Manyar Km. 11, <br>Manyar-Gresik, East Java 61151<br>
-                                            <a class="clicks_map" target="_blank"
-                                                href="https://www.google.com/maps/place/JIIPE+Office/@-7.0856958,112.6046212,17z/data=!4m5!3m4!1s0x0:0xaee7dea2e8cb80a3!8m2!3d-7.0862954!4d112.6030116">Look
-                                                on Google Map</a>
+                                            <b>{{ $contactData->translation->office_name ?? 'PT. Berkah Kawasan Manyar Sejahtera' }}</b><br>
+                                            {!! nl2br(e($contactData->translation->address ?? 'Jl. Raya Manyar Km. 11, Manyar-Gresik, East Java 61151')) !!}<br>
+                                            @if ($contactData->translation->map_link)
+                                                <a class="clicks_map" target="_blank"
+                                                    href="{{ $contactData->translation->map_link }}">
+                                                    Look on Google Map
+                                                </a>
+                                            @endif
                                         </p>
                                     </div>
                                 </div>
@@ -95,7 +102,12 @@
                                     </div>
                                     <div class="sn_text d-inline-block align-top pl-1">
                                         <span class="pb-3 d-block">Telephone</span>
-                                        <p class="pt-1"><a href="tel:+623198540999">+62 31 985 409 99</a></p>
+                                        <p class="pt-1">
+                                            <a
+                                                href="tel:{{ str_replace([' ', '-', '(', ')'], '', $contactData->translation->phone ?? '+6231985409 99') }}">
+                                                {{ $contactData->translation->phone ?? '+62 31 985 409 99' }}
+                                            </a>
+                                        </p>
                                     </div>
                                 </div>
                             </div>
