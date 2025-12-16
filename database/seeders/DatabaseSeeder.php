6c1acef47e6f988e35c6f7b5c2bb90ee17224029
irvansindy <irvanmuhammad22@gmail.com>
2025-12-16 13:20:31 +0700

    detailing database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,6 +35,8 @@ class DatabaseSeeder extends Seeder
             CareerSeeder::class,
             GallerySeeder::class,
             AboutUsSeeder::class,
+            AboutUsContentSeeder::class,
+            ContactOverviewSeeder::class,
         ]);
     }
 }
