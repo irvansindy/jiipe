4161ed0995608edfcd8e616385dff9725a17edc2
irvansindy <irvanmuhammad22@gmail.com>
2026-02-04 15:16:31 +0700

    fix css inline news read more

--- resources/views/layouts/client/blog/sections/content.blade.php
+++ resources/views/layouts/client/blog/sections/content.blade.php
@@ -195,6 +195,32 @@
         </div>
     </div>
 </section>
+@push('css')
+<style>
+.lebih p {
+    display: inline-flex !important;
+    align-items: center !important;
+    margin: 0 !important;
+    vertical-align: middle !important;
+}
+
+.lebih p span {
+    display: inline-flex !important;
+    align-items: center !important;
+    margin-left: 8px;
+    line-height: 1 !important;
+    vertical-align: middle !important;
+}
+
+.lebih p span img {
+    display: block !important;
+    margin: 0 !important;
+    padding: 0 !important;
+    vertical-align: middle !important;
+    max-height: 1em;
+}
+</style>
+@endpush
 @push('js')
 <script type="text/javascript">
     $(document).ready(function() {
